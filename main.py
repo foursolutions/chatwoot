bce8ca3e6c554ef078aa5caadcf0c5728d2ac219
Nathan <sales@foursolutions.sg>
2025-05-27 02:21:25 +0800

    Switch to main_menu_v2 template

--- main.py
+++ main.py
@@ -157,8 +157,8 @@ def reset_conversation(to, customer_name):
 
 def send_main_menu(to, customer_name):
     namespace = "94d66366_9ec1_43a3_a84c_46039bd33ef5"
-    template_name = "main_menu"
-    send_template_message(to, "main_menu", "94d66366_9ec1_43a3_a84c_46039bd33ef5", [customer_name])
+    template_name = "main_menu_v2"  # use your new approved version
+    send_template_message(to, template_name, namespace, [customer_name])
 
 # -------------------------------
 # Admin Command Handling
